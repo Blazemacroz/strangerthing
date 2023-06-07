601ad26d37e78deac2436c3917dd5a286630b84c
Sage <blazemacroz@gmail.com>
2023-06-07 18:00:36 -0500

    added node modules

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+export COHORT_NAME = '2303-FTB-ET-WEB-PT'
+export const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`
+
+
+const fetchPosts = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/posts`)
+  
+      const result = await response.json();
+      console.log(result);
+      return result
+    } catch (err) {
+      console.error(err);
+    }
+  }
